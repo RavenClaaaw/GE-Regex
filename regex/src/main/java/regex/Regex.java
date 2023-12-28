fffdb25070960fd6da3e13330ca6891a6dee20f3
Roop Kumar Lala <ui20cs60@iiitsurat.ac.in>
2023-12-28 22:50:20 +0530

    Valid Last Name

--- regex/src/main/java/regex/Regex.java
+++ regex/src/main/java/regex/Regex.java
@@ -5,10 +5,22 @@ import java.util.regex.Pattern;
 
 public class Regex {
     public static void main(String[] args) {
+        Regex regex = new Regex();
+        String input = "Lala";
+        System.out.println("PATTERN MATCH:- " + regex.validLastName(input));
+    }
+
+    public boolean validFirstName(String input){
+        Pattern pattern = Pattern.compile("[A-Z][\\w]{2,}");
+
+        Matcher matcher = pattern.matcher(input);
+        return matcher.matches();
+    }
+
+    public boolean validLastName(String input){
         Pattern pattern = Pattern.compile("[A-Z][\\w]{2,}");
         
-        String input = "RoOp";
         Matcher matcher = pattern.matcher(input);
-        System.out.println("PATTERN MATCH:- " + matcher.matches());
+        return matcher.matches();
     }
 }
