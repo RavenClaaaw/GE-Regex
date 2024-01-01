5e3fb9038e4d579948a2e41f9d599644a5040faa
Roop Kumar Lala <ui20cs60@iiitsurat.ac.in>
2024-01-01 23:24:24 +0530

    Parameterized Test

--- regex/src/test/java/Junit.java
+++ regex/src/test/java/Junit.java
@@ -2,6 +2,9 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import main.java.regex.Regex;
 
 public class Junit {
@@ -73,5 +76,19 @@ public class Junit {
         String input = "roop@apple";
         Regex regex = new Regex();
         assertFalse(regex.validPassword(input));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com", "abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com"})
+    public void testAllEmailHappy(String input){
+        Regex regex = new Regex();
+        assertTrue(regex.validEmail(input));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"abc", "abc@.com.my", "abc123@gmail.a", "abc123@.com", "abc123@.com.com", ".abc@abc.com", "abc()*@gmail.com", "abc@%*.com", "abc..2002@gmail.com", "abc.@gmail.com", "abc@abc@gmail.com", "abc@gmail.com.1a", "abc@gmail.com.aa.uu"})
+    public void testAllEmailSad(String input){
+        Regex regex = new Regex();
+        assertFalse(regex.validEmail(input));
     }
 }
